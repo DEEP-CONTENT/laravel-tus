70f0c9312e6339c2ef342c872fc64ee37b99ccea
Milanowicz <amaru-jive@gmx.de>
2025-06-04 16:50:33 +0200

    Edit Interface classes

--- src/Contracts/TusFileFactoryInterface.php
+++ src/Contracts/TusFileFactoryInterface.php
@@ -6,7 +6,11 @@ namespace KalynaSolutions\Tus\Contracts;
 
 interface TusFileFactoryInterface
 {
-    public static function create(?string $id = null, int $size = 0, ?string $rawMetadata = null): TusFileInterface;
+    public static function create(
+        ?string $id = null,
+        int $size = 0,
+        ?string $rawMetadata = null
+    ): static;
 
-    public static function find(string $id): TusFileInterface;
+    public static function find(string $id): static;
 }
